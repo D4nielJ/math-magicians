3f775813f164904fc1324709b7cdf15fc1d7310f
d4nielj <d4niel.djm@gmail.com>
2021-08-10 01:53:00 -0500

    Add App.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calc from './components/calc';
+import App from './app';
 import 'modern-css-reset';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calc />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
