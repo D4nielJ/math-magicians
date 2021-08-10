1bd1cbef099f94347a0b8218a8e54e45092d419c
d4nielj <d4niel.djm@gmail.com>
2021-08-10 14:16:12 -0500

    Add logic by Microverse

--- src/components/calc.js
+++ src/components/calc.js
@@ -10,12 +10,29 @@ class Calc extends React.Component {
     this.state = {
       displayValue: '0',
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      displayValue: '1',
+    });
+  }
+
+  handleKeyDown(e) {
+    console.log(e.keyCode, this);
   }
 
   render() {
     const { displayValue } = this.state;
     return (
-      <div className="calc">
+      <div
+        className="calc"
+        onClick={this.handleClick}
+        onKeyDown={this.handleKeyDown}
+        aria-hidden="true"
+      >
         <Display value={displayValue} />
         <ButtonsContainer />
       </div>
