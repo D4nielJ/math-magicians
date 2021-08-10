32a77b24920eacc3631a1a1fbf676f8799907ac0
d4nielj <d4niel.djm@gmail.com>
2021-08-10 01:40:53 -0500

    Add styles for colored and column span btns

--- src/components/calc/buttonsContainer.js
+++ src/components/calc/buttonsContainer.js
@@ -10,22 +10,22 @@ class ButtonsContainer extends React.Component {
         <CalcButton name="clear" value="AC" />
         <CalcButton name="sign" value="+/-" />
         <CalcButton name="percentage" value="%" />
-        <CalcButton name="division" value="÷" colored={true} />
+        <CalcButton name="division" value="÷" colored />
         <CalcButton name="number" value="7" />
         <CalcButton name="number" value="8" />
         <CalcButton name="number" value="9" />
-        <CalcButton name="mult" value="x" colored={true} />
+        <CalcButton name="mult" value="x" colored />
         <CalcButton name="number" value="4" />
         <CalcButton name="number" value="5" />
         <CalcButton name="number" value="6" />
-        <CalcButton name="substr" value="-" colored={true} />
+        <CalcButton name="substr" value="-" colored />
         <CalcButton name="number" value="1" />
         <CalcButton name="number" value="2" />
         <CalcButton name="number" value="3" />
-        <CalcButton name="sum" value="+" colored={true} />
+        <CalcButton name="sum" value="+" colored />
         <CalcButton name="number" value="0" span={2} />
         <CalcButton name="floating" value="." />
-        <CalcButton name="result" value="=" colored={true} />
+        <CalcButton name="result" value="=" colored />
       </div>
     );
   }
