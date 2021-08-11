f5699db0a6c133d58ab2af84a2e94c55371d0ca0
d4nielj <d4niel.djm@gmail.com>
2021-08-11 17:29:42 -0500

    Add styles for new functionality of the display

--- src/components/calc/display.js
+++ src/components/calc/display.js
@@ -3,25 +3,19 @@ import PropTypes from 'prop-types';
 
 const Display = (props) => {
   const { total, next, operation } = props.state;
-  let display = 0;
-  let holder = '';
-  if (total) {
-    display = total;
+  const { operationChar, holder, display } = displayLogic(total, next, operation);
+  let classOperation = 'display__operation';
+  if (operationChar === 0) {
+    classOperation = 'display__operation no-opacity';
   }
-  if (next) {
-    display = next;
-  }
-  if (total && operation) {
-    display = '';
-    holder = total;
-    if (next) {
-      display = next;
-    }
+  let classHolder = 'display__holder';
+  if (holder === 0) {
+    classHolder = 'display__holder no-opacity';
   }
   return (
     <div className="calc__display">
-      <div className="display__operation">{operation}</div>
-      <div className="display__total">{holder}</div>
+      <div className={classOperation}>{operationChar}</div>
+      <div className={classHolder}>{holder}</div>
       <div className="display__next">{display}</div>
     </div>
   );
@@ -38,6 +32,29 @@ Display.defaultProps = {
   total: '',
   next: '',
   operation: '',
+};
+
+const displayLogic = (total, next, operation) => {
+  let display = 0;
+  let holder = 0;
+  let operationChar = 0;
+  if (operation) {
+    operationChar = operation;
+  }
+  if (total) {
+    display = total;
+  }
+  if (next) {
+    display = next;
+  }
+  if (total && operation) {
+    display = ' ';
+    holder = total;
+    if (next) {
+      display = next;
+    }
+  }
+  return { display, holder, operationChar };
 };
 
 export { Display as default };
