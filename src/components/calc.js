3e11a0ff1b9c1b8b58a80e12318c4c8694f291cb
d4nielj <d4niel.djm@gmail.com>
2021-08-11 16:07:41 -0500

    Finish refactor of Calc.js to functional component

--- src/components/calc.js
+++ src/components/calc.js
@@ -1,38 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Display from './calc/display';
 import ButtonsContainer from './calc/buttonsContainer';
 import calculate from './calc/logic/calculate';
 import './calc.css';
 
-const updateState = (obj, key) => {
-  const newObj = calculate(obj, key);
-  return newObj;
-};
-
-const handleClick = (obj, e) => {
-  updateState(obj, e.target.dataset.name);
-};
-
-const handleKeyDown = (e) => {
-  e.preventDefault();
-};
-
 const Calc = () => {
-  const [total, setTotal] = useState(null);
-  const [next, setNext] = useState(null);
-  const [operation, setOperation] = useState(null);
-  const [calc, setCalc] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+  const [calc, setCalc] = useState({ total: null, next: null, operation: null });
 
-  useEffect(handleClick(calc, e));
+  const updateState = async (obj, key) => {
+    console.log(obj, key);
+    let { total, next, operation } = await calculate(obj, key);
+    if (total === undefined) {
+      total = obj.total;
+    }
+    if (next === undefined) {
+      next = obj.next;
+    }
+    if (operation === undefined) {
+      operation = obj.operation;
+    }
+    setCalc({ total, next, operation });
+  };
+
+  const handleClick = (obj, e) => {
+    updateState(obj, e.target.dataset.name);
+  };
+
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="calc">
-      <Display total={total} next={next} />
-      <ButtonsContainer click={handleClick} keyDown={handleKeyDown} />
+      <Display total={calc.total} next={calc.next} />
+      <ButtonsContainer click={(e) => handleClick(calc, e)} keyDown={handleKeyDown} />
     </div>
   );
 };
