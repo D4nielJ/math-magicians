1a28af0ad65ede2265f21696c9e7058609e20cd3
d4nielj <d4niel.djm@gmail.com>
2021-08-12 18:22:48 -0500

    Fix linter errors

--- src/components/calc/warning.js
+++ src/components/calc/warning.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Warning = (props) => {
   const { warning } = props;
   const warningValue = 'DO NOT DIVIDE BY 0! AGAIN, DO NOT DIVIDE BY 0!!!';
@@ -9,10 +11,18 @@ const Warning = (props) => {
 
   return (
     <div className={classList}>
-      <i className="fas fa-radiation"></i>
+      <i className="fas fa-radiation" />
       <p>{warningValue}</p>
     </div>
   );
+};
+
+Warning.propTypes = {
+  warning: PropTypes.bool,
+};
+
+Warning.defaultProps = {
+  warning: false,
 };
 
 export default Warning;
